696d3877792a279896d48f137f7dd2f9c3f0b92d
Jason Suarez <swarecito@gmail.com>
2023-01-31 20:00:09 +0100

    changed css body box-sizing and added properties to meta tag

--- .history/src/pages/_app_20230131195855.tsx
+++ .history/src/pages/_app_20230131195855.tsx
@@ -14,7 +14,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
       <Head>
-        <meta name="viewport" content="width=device-width"  initial-scale="1.0" viewport-fit="cover"/>
+        <meta name="viewport" content="width=device-width"  initial-scale=1.0, viewport-fit="cover"/>
       </Head>
 
       <main className={inter.className}>
